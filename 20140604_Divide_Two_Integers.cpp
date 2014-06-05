4a93af2a98c18d2e5ba62839a78cb76c788ec976
ArchSun <northernsun@126.com>
2014-06-05 15:29:25 +0800

    Update 20140604_Divide_Two_Integers.cpp

--- 20140604_Divide_Two_Integers.cpp
+++ 20140604_Divide_Two_Integers.cpp
@@ -1,19 +1,21 @@
-// a copy
-
 class Solution {
 public:
     int divide(int dividend, int divisor) {
         // Note: The Solution object is instantiated only once.
-        long long a = abs((double)dividend);
-		long long b = abs((double)divisor);
-		long long res = 0;
-		while( a >= b) {
-			long long c = b;
-			for( int i = 0; a >= c; i++, c <<=1) {
-				a -= c;
-				res += 1<<i;
-			}
-		}
-		return ( (dividend ^ divisor) >> 31)? 0-res : res;
+        unsigned a = dividend;
+        unsigned b = divisor;
+        unsigned ret = 0;
+        long long c = b;
+        int i = 0;
+        while( a>=b) {
+      		c = b;
+      		for( i=0; a>=c; i++ ) {
+      			a -= c;
+      			ret += 1<<i;
+      			c <<= 1;
+      		}
+        }
+        
+        return (dividend^divisor)>>31? 0-ret: ret;
     }
 };
